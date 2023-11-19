97680115892c8bdf92b00773fd34b0d6ec212752
“Abhishek <abhishek.shyam@brightchamps.com>
2023-11-19 09:34:14 +0530

    Everything fixed

--- src/components/RepoWidget/RepoWidget.tsx
+++ src/components/RepoWidget/RepoWidget.tsx
@@ -1,18 +1,22 @@
-import NewTabIcon from '../../assets/new-tab.svg'
+import NewTabIcon from "../../assets/new-tab.svg";
 
-import './RepoWidget.scss'
+import "./RepoWidget.scss";
 
 function RepoWidget() {
   const handleClick = () => {
-    window.open('https://github.com/tboulis/tasosboulis-com', '_blank', 'noopener noreferrer');
-  }
-  
+    window.open(
+      "https://github.com/tboulis/tasosboulis-com",
+      "_blank",
+      "noopener noreferrer"
+    );
+  };
+
   return (
-    <div className='RepoWidget' onClick={handleClick}>
-      {'Source code'}
-      <img src={NewTabIcon} alt='Source code' rel='noopener noreferrer' height={20}/>
+    <div className="RepoWidget" onClick={handleClick}>
+      {/* {'Source code'}
+      <img src={NewTabIcon} alt='Source code' rel='noopener noreferrer' height={20}/> */}
     </div>
-  )
+  );
 }
 
-export default RepoWidget
+export default RepoWidget;
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,15 +1,15 @@
-import Layout from '../Layout/Layout'
-import RepoWidget from '../../components/RepoWidget/RepoWidget'
+import Layout from "../Layout/Layout";
+import RepoWidget from "../../components/RepoWidget/RepoWidget";
 
-import './App.scss'
+import "./App.scss";
 
 function App() {
   return (
-    <div className='App'>
-      <RepoWidget />
+    <div className="App">
+      {/* <RepoWidget /> */}
       <Layout />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
